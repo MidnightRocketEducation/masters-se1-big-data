9b3a9c40b0185f1732563648ac32d2db1442a0ce
MidnightRocket <git@midnightrocket.dev>
2025-12-11 17:15:48 +0100

    Add avro-swift dependency
    Needed to pin to a specific revision (commit) which contains updated
    dependency of swift-syntax 602. To resolve conflict between avro-swift
    and CodingKeysGenerator

--- yelp-producer/server/Package.swift
+++ yelp-producer/server/Package.swift
@@ -10,9 +10,10 @@ let package = Package(
 	],
 	dependencies: [
 		.package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
-		.package(url: "https://github.com/zijievv/CodingKeysGenerator", from: "0.3.1"),
 		.package(url: "https://github.com/swift-server/swift-kafka-client", branch: "main"),
 		.package(url: "https://github.com/swift-server/swift-service-lifecycle.git", from: "2.0.0"),
+		.package(url: "https://github.com/zijievv/CodingKeysGenerator", revision: "0a963c41d6d19f9e1521d2badc3f613b2bb92217"),
+		.package(url: "https://github.com/flexlixrup/avro-swift", revision: "2f3fe318f5ea440be475a2a255a564322c849fe6"),
 	],
 	targets: [
 		// Targets are the basic building blocks of a package, defining a module or a test suite.
