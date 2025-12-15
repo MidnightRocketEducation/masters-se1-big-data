e9e8d75f8d5472859ac9d450c9c8a5a8e19ca629
MidnightRocket <git@midnightrocket.dev>
2025-12-15 03:05:54 +0100

    Update avro

--- yelp-producer/server/Package.swift
+++ yelp-producer/server/Package.swift
@@ -13,7 +13,7 @@ let package = Package(
 		.package(url: "https://github.com/swift-server/swift-kafka-client", branch: "main"),
 		.package(url: "https://github.com/swift-server/swift-service-lifecycle.git", from: "2.0.0"),
 		.package(url: "https://github.com/zijievv/CodingKeysGenerator", revision: "0a963c41d6d19f9e1521d2badc3f613b2bb92217"),
-		.package(url: "https://github.com/flexlixrup/avro-swift", revision: "2f3fe318f5ea440be475a2a255a564322c849fe6"),
+		.package(url: "https://github.com/flexlixrup/avro-swift", revision: "035628e09b7842623dfe1733d4aa0934581d2ec0"),
 	],
 	targets: [
 		// Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -25,6 +25,7 @@ let package = Package(
 				.product(name: "CodingKeysGenerator", package: "CodingKeysGenerator"),
 				.product(name: "ServiceLifecycle", package: "swift-service-lifecycle"),
 				.product(name: "Kafka", package: "swift-kafka-client"),
+				.product(name: "Avro", package: "avro-swift"),
 			]
 		),
 	]
