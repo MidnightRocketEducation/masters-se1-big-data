734b7aa58f20645eb337446a550935a2a40c6afb
MattisJensen <matjen@mail.de>
2025-12-22 14:51:02 +0100

    import clean up

--- world-clock-producer/src/main/java/dk/sdu/bigdata/world/clock/producer/WorldClockProducer.java
+++ world-clock-producer/src/main/java/dk/sdu/bigdata/world/clock/producer/WorldClockProducer.java
@@ -1,6 +1,5 @@
 package dk.sdu.bigdata.world.clock.producer;
 
-import dk.sdu.bigdata.world.clock.producer.infrastructure.PublishCurrentTimeScheduler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
