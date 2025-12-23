4ec5a291eb77f0ab6a1f917a419c6670765a6f8b
MattisJensen <matjen@mail.de>
2025-12-23 12:01:25 +0100

    time topic consumer

--- weather-producer-java/src/main/java/dk/sdu/bigdata/weather/producer/presentation/WeatherProducerController.java
+++ weather-producer-java/src/main/java/dk/sdu/bigdata/weather/producer/presentation/WeatherProducerController.java
@@ -1,5 +1,7 @@
 package dk.sdu.bigdata.weather.producer.presentation;
 
+import dk.sdu.bigdata.weather.producer.application.MessagePublisher;
+import dk.sdu.bigdata.weather.producer.application.TimeProvider;
 import dk.sdu.bigdata.weather.producer.application.WeatherDataImportUseCase;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,9 +14,11 @@ import java.time.Instant;
 @RequestMapping("/api/v1/weather-producer")
 public class WeatherProducerController {
     private final WeatherDataImportUseCase weatherDataImportUseCase;
+    private final TimeProvider timeProvider;
 
-    public WeatherProducerController(WeatherDataImportUseCase weatherDataImportUseCase) {
+    public WeatherProducerController(WeatherDataImportUseCase weatherDataImportUseCase, TimeProvider timeProvider) {
         this.weatherDataImportUseCase = weatherDataImportUseCase;
+        this.timeProvider = timeProvider;
     }
 
     @PostMapping("/produce-from-path") //usally called with "/data"
@@ -22,6 +26,16 @@ public class WeatherProducerController {
         Path dataRoot = Path.of("/data");
         weatherDataImportUseCase.importWeatherData(dataRoot);
         return ResponseEntity.status(HttpStatus.ACCEPTED).build();
+    }
+
+    @GetMapping("/current-time")
+    public ResponseEntity<String> getCurrentTime() {
+        if (timeProvider.getCurrentTime().isPresent()) {
+            Instant currentTime = timeProvider.getCurrentTime().get();
+            return ResponseEntity.ok(currentTime.toString());
+        } else {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
     }
 }
 
